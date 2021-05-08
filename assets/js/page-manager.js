chrishayesmu/XCOM-Companion

commit 7fc6a17fbc205a15063aaf7874b4cf50ffd4ee2b

Refactor how pages are laid out so that the title element is handled by PageManager, and the page body doesn't include the containing section tags anymore

assets/js/page-manager.js
@@ -34,11 +34,12 @@ for (let i = 0; i < appPages.length; i++) {
 class PageManager {
     static instance = null;
 
-    constructor(pageContentHolder) {
+    constructor(pageContentHolder, pageTitleHolder) {
         this.currentPage = null;
         this.currentTooltipTarget = null;
         this.maxPageHistorySize = 50;
-        this.pageContentHolder = pageContentHolder;
+        this._pageContentHolder = pageContentHolder;
+        this._pageTitleHolder = pageTitleHolder;
         this.pageHistory = [];
         this.pagePreview = null;
         this._showTooltip = false;
@@ -82,11 +83,9 @@ class PageManager {
         this._unloadCurrentPage();
         this.hidePagePreviewTooltip();
         this.currentPage = pagesById[pageId];
-        const pageDocument = await this.currentPage.load(data);
 
-        // Clear out the contents of the hosting element and replace them with this page
-        this.pageContentHolder.innerHTML = "";
-        this.pageContentHolder.appendChild(pageDocument);
+        const pageDocument = await this.currentPage.load(data);
+        this._loadPageIntoDom(pageDocument);
     }
 
     loadPageForData(data) {
@@ -100,11 +99,9 @@ class PageManager {
 
                     this.hidePagePreviewTooltip();
                     this._unloadCurrentPage();
-                    this.currentPage = page;
 
-                    // Clear out the contents of the hosting element and replace them with this page
-                    this.pageContentHolder.innerHTML = "";
-                    this.pageContentHolder.appendChild(pageDocument);
+                    this.currentPage = page;
+                    this._loadPageIntoDom(pageDocument);
                 })
 
                 return;
@@ -188,6 +185,25 @@ class PageManager {
         this.showPagePreviewTooltip(event.target.dataset.pageOnClick, data, targetElementRect);
     }
 
+    async _loadPageIntoDom(pageDocument) {
+        const documentBody = await pageDocument.body;
+
+        // Replace the header content entirely
+        this._pageTitleHolder.innerHTML = "";
+
+        if (pageDocument.title.icon) {
+            const img = document.createElement("img");
+            img.src = pageDocument.title.icon;
+
+            this._pageTitleHolder.appendChild(img);
+        }
+
+        this._pageTitleHolder.innerHTML += pageDocument.title.text;
+
+        // Clear out the contents of the hosting element and replace them with this page
+        this._pageContentHolder.innerHTML = documentBody.outerHTML;
+    }
+
     _pushToHistory(historyState) {
         while (this.pageHistory.length >= this.maxPageHistorySize) {
             // Remove the oldest entries until the history is small enough
@@ -239,8 +255,9 @@ class PageManager {
     }
 }
 
-const pageContentHolder = document.getElementById("page-content");
-PageManager.instance = new PageManager(pageContentHolder);
+const pageContentHolder = document.getElementById("page-body-container");
+const pageTitleHolder = document.getElementById("page-title-container");
+PageManager.instance = new PageManager(pageContentHolder, pageTitleHolder);
 
 Search.onDomReady();
 PageManager.instance.loadPage("home-page", { });

assets/css/section.css
@@ -2,7 +2,7 @@
 
 .section {
   position: absolute;
-  top: 0;
+  top: 6rem;
   left: 0;
   right: 0;
   bottom: 0;

assets/html/templates/pages/class-selection-page.html
@@ -1,13 +1,5 @@
 <template id="template-class-selection-page">
-    <section id="class-selection-section" class="section js-section">
-        <header class="section-header">
-            <div class="section-wrapper">
-                <img src="assets/img/misc-icons/xcom_logo.png" /> Perk Trees
-            </div>
-        </header>
-
-        <div class="section-content">
-            <div id="class-selection-content" class="soldier-class-grid"></div>
-        </div>
-    </section>
+    <div class="section-content">
+        <div id="class-selection-content" class="soldier-class-grid"></div>
+    </div>
 </template>

assets/html/templates/pages/foundry-project-display-page.html
@@ -9,59 +9,51 @@
 </template>
 
 <template id="template-foundry-project-display-page">
-    <section class="section js-section">
-        <header class="section-header">
-            <div class="section-wrapper">
-                <img src="assets/img/misc-icons/foundry.png" /> Foundry Project
+    <div id="foundry-project-content-section" class="section-content">
+        <div id="foundry-project-header-row" class="flex-single-row">
+            <div id="foundry-project-image-container">
+                <img src="" />
             </div>
-        </header>
-
-        <div id="foundry-project-content-section" class="section-content">
-            <div id="foundry-project-header-row" class="flex-single-row">
-                <div id="foundry-project-image-container">
-                    <img src="" />
-                </div>
-                <div id="foundry-project-description-container">
-                    <div id="foundry-project-name"></div>
-                    <div id="foundry-project-description"></div>
-                    <div id="foundry-project-research-credits"></div>
-                </div>
-            </div>
-            <div id="foundry-project-second-row" class="flex-single-row">
-                <div class="foundry-project-left-column">
-                    <div class="foundry-project-heading">Cost</div>
-                    <div class="foundry-project-column-body" id="foundry-project-cost-container">
-                    </div>
-                </div>
-                <div class="foundry-project-right-column">
-                    <div class="foundry-project-heading">Project Duration</div>
-                    <div class="foundry-project-column-body">
-                        <div id="foundry-project-time-container">
-                            <img class="foundry-project-inline-icon" src="assets/img/misc-icons/time.png" />
-                            <span id="foundry-project-time"></span>&nbsp;days
-                        </div>
-                        <div class="flex-column-bottom flex-grow">
-                            <div>
-                                Displayed project time is based on having <span id="foundry-project-num-engineers"></span> engineers, 0 <a>Workshops</a> and 0 adjacency bonuses.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="flex-single-row">
-                <div class="foundry-project-left-column">
-                    <div class="foundry-project-heading">Research Prerequisite</div>
-                    <div class="foundry-project-column-body" id="foundry-project-prerequisites"></div>
-                </div>
-                <div class="foundry-project-right-column">
-                    <div class="foundry-project-heading">Unlocked Items</div>
-                    <div class="foundry-project-column-body" id="foundry-project-unlocks"></div>
-                </div>
-            </div>
-            <div id="foundry-project-main-row" class="flex-single-row">
-                <div class="foundry-project-heading" id="foundry-project-benefits-heading">Project Benefits</div>
-                <div id="foundry-project-benefits"></div>
+            <div id="foundry-project-description-container">
+                <div id="foundry-project-name"></div>
+                <div id="foundry-project-description"></div>
+                <div id="foundry-project-research-credits"></div>
             </div>
         </div>
-    </section>
+        <div id="foundry-project-second-row" class="flex-single-row">
+            <div class="foundry-project-left-column">
+                <div class="foundry-project-heading">Cost</div>
+                <div class="foundry-project-column-body" id="foundry-project-cost-container">
+                </div>
+            </div>
+            <div class="foundry-project-right-column">
+                <div class="foundry-project-heading">Project Duration</div>
+                <div class="foundry-project-column-body">
+                    <div id="foundry-project-time-container">
+                        <img class="foundry-project-inline-icon" src="assets/img/misc-icons/time.png" />
+                        <span id="foundry-project-time"></span>&nbsp;days
+                    </div>
+                    <div class="flex-column-bottom flex-grow">
+                        <div>
+                            Displayed project time is based on having <span id="foundry-project-num-engineers"></span> engineers, 0 <a>Workshops</a> and 0 adjacency bonuses.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex-single-row">
+            <div class="foundry-project-left-column">
+                <div class="foundry-project-heading">Research Prerequisite</div>
+                <div class="foundry-project-column-body" id="foundry-project-prerequisites"></div>
+            </div>
+            <div class="foundry-project-right-column">
+                <div class="foundry-project-heading">Unlocked Items</div>
+                <div class="foundry-project-column-body" id="foundry-project-unlocks"></div>
+            </div>
+        </div>
+        <div id="foundry-project-main-row" class="flex-single-row">
+            <div class="foundry-project-heading" id="foundry-project-benefits-heading">Project Benefits</div>
+            <div id="foundry-project-benefits"></div>
+        </div>
+    </div>
 </template>

assets/html/templates/pages/foundry-projects-browse-page.html
@@ -1,85 +1,77 @@
 <template id="template-foundry-projects-browse-page">
-    <section class="section js-section">
-        <header class="section-header">
-            <div class="section-wrapper">
-                <img src="assets/img/misc-icons/foundry.png" /> Available Foundry Projects
-            </div>
-        </header>
+    <div id="foundry-project-browse-content-section" class="section-content">
+        <details>
+            <summary>Aerospace (14)</summary>
+            <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
+            <foundry-project-browse-preview projectId="foundry_aircraft_boosters"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_advanced_repair"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_armored_fighters"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_elerium_afterburners"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_improved_avionics"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_penetrator_weapons"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_phoenix_coilguns"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_stealth_satellites"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_supercapacitors"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_super_skyranger"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_ufo_countermeasures"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_ufo_scanners"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_ufo_tracking"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_wingtip_sparrowhawks"></foundry-project-browse-preview>
+        </details>
 
-        <div id="foundry-project-browse-content-section" class="section-content">
-            <details>
-                <summary>Aerospace (14)</summary>
-                <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
-                <foundry-project-browse-preview projectId="foundry_aircraft_boosters"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_advanced_repair"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_armored_fighters"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_elerium_afterburners"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_improved_avionics"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_penetrator_weapons"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_phoenix_coilguns"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_stealth_satellites"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_supercapacitors"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_super_skyranger"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_ufo_countermeasures"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_ufo_scanners"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_ufo_tracking"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_wingtip_sparrowhawks"></foundry-project-browse-preview>
-            </details>
+        <details>
+            <summary>Equipment Upgrades (8)</summary>
+            <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
+            <foundry-project-browse-preview projectId="foundry_advanced_flight"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_drone_capture"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_improved_arc_thrower"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_improved_medikit"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_new_combat_systems"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_psi_warfare_systems"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_scope_upgrade"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_tactical_rigging"></foundry-project-browse-preview>
+        </details>
 
-            <details>
-                <summary>Equipment Upgrades (8)</summary>
-                <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
-                <foundry-project-browse-preview projectId="foundry_advanced_flight"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_drone_capture"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_improved_arc_thrower"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_improved_medikit"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_new_combat_systems"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_psi_warfare_systems"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_scope_upgrade"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_tactical_rigging"></foundry-project-browse-preview>
-            </details>
+        <details>
+            <summary>MEC and SHIV Upgrades (10)</summary>
+            <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
+            <foundry-project-browse-preview projectId="foundry_advanced_repair"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_advanced_servomotors"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_field_repairs"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_jellied_elerium"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_mec_close_combat"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_mec_warfare_systems"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_mechanized_unit_defenses"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_sentinel_drone"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_shaped_armor"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_shiv_suppression"></foundry-project-browse-preview>
+        </details>
 
-            <details>
-                <summary>MEC and SHIV Upgrades (10)</summary>
-                <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
-                <foundry-project-browse-preview projectId="foundry_advanced_repair"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_advanced_servomotors"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_field_repairs"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_jellied_elerium"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_mec_close_combat"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_mec_warfare_systems"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_mechanized_unit_defenses"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_sentinel_drone"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_shaped_armor"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_shiv_suppression"></foundry-project-browse-preview>
-            </details>
+        <details>
+            <summary>Miscellaneous (6)</summary>
+            <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
+            <foundry-project-browse-preview projectId="foundry_advanced_surgery"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_alien_metallurgy"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_alien_nucleonics"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_base_security_armor"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_base_security_weapons"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_improved_salvage"></foundry-project-browse-preview>
+        </details>
 
-            <details>
-                <summary>Miscellaneous (6)</summary>
-                <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
-                <foundry-project-browse-preview projectId="foundry_advanced_surgery"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_alien_metallurgy"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_alien_nucleonics"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_base_security_armor"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_base_security_weapons"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_improved_salvage"></foundry-project-browse-preview>
-            </details>
-
-            <details>
-                <summary>Weapon Upgrades (9)</summary>
-                <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
-                <foundry-project-browse-preview projectId="foundry_alien_grenades"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_ammo_conservation"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_enhanced_ballistics"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_enhanced_lasers"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_enhanced_plasma"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_mag_pistols"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_quenchguns"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_rail_pistols"></foundry-project-browse-preview>
-                <foundry-project-browse-preview projectId="foundry_reflex_pistols"></foundry-project-browse-preview>
-            </details>
-        </div>
-    </section>
+        <details>
+            <summary>Weapon Upgrades (9)</summary>
+            <foundry-project-browse-preview-header></foundry-project-browse-preview-header>
+            <foundry-project-browse-preview projectId="foundry_alien_grenades"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_ammo_conservation"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_enhanced_ballistics"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_enhanced_lasers"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_enhanced_plasma"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_mag_pistols"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_quenchguns"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_rail_pistols"></foundry-project-browse-preview>
+            <foundry-project-browse-preview projectId="foundry_reflex_pistols"></foundry-project-browse-preview>
+        </details>
+    </div>
 </template>
 
 <template id="template-foundry-projects-browse-preview">

assets/html/templates/pages/home-page.html
@@ -1,39 +1,31 @@
 <template id="template-home-page">
-    <section id="class-selection-section" class="section js-section">
-        <header class="section-header">
-            <div class="section-wrapper">
-                Home Page
+    <div class="section-content" style="padding: 1rem 1.25rem">
+        <h1>Welcome to XCOM Companion!</h1>
+        <p>You can get started by selecting a category on the left. Alternatively, search for something directly using the search box in the top left. Here's a few tips:</p>
+        <ul>
+            <li>At any time, pressing Ctrl+F will focus the search box. Press enter to submit a search.</li>
+            <li>If your search exactly matches a game term, or matches only one game term, you'll be sent straight there. For example, searching "xenob" will always bring you to the <a data-page-on-click="tech-details-page" data-pagearg-tech-id="research_xenobiology">Xenobiology</a> research.</li>
+            <li>If you're looking at an item's stats and you're not sure what a column means, hover over the image.</li>
+            <li>You can get back to this page by clicking the <span style="text-transform: uppercase">XCOM COMPANION</span> logo in the top left of the window.</li>
+        </ul>
+        <p>If you ever have any feedback, bug reports, or other comments, feel free to stop by the <a href="https://github.com/chrishayesmu/XCOM-Companion/issues">GitHub repo</a> and open an issue.</p>
+
+        <br /><br />
+        <div id="changelog">
+            <h2>Changelog</h2>
+            <div class="changelog-version">
+                <h3>Version 0.1.1</h3>
+                <ul>
+                    <li>Added stats tables in item tooltip for a few item types which were missing them</li>
+                    <li>Fixed an issue where stats tables in item tooltips could extend beyond the tooltip boundaries</li>
+                </ul>
             </div>
-        </header>
-
-        <div class="section-content" style="padding: 1rem 1.25rem">
-            <h1>Welcome to XCOM Companion!</h1>
-            <p>You can get started by selecting a category on the left. Alternatively, search for something directly using the search box in the top left. Here's a few tips:</p>
-            <ul>
-                <li>At any time, pressing Ctrl+F will focus the search box. Press enter to submit a search.</li>
-                <li>If your search exactly matches a game term, or matches only one game term, you'll be sent straight there. For example, searching "xenob" will always bring you to the <a data-page-on-click="tech-details-page" data-pagearg-tech-id="research_xenobiology">Xenobiology</a> research.</li>
-                <li>If you're looking at an item's stats and you're not sure what a column means, hover over the image.</li>
-                <li>You can get back to this page by clicking the <span style="text-transform: uppercase">XCOM COMPANION</span> logo in the top left of the window.</li>
-            </ul>
-            <p>If you ever have any feedback, bug reports, or other comments, feel free to stop by the <a href="https://github.com/chrishayesmu/XCOM-Companion/issues">GitHub repo</a> and open an issue.</p>
-
-            <br /><br />
-            <div id="changelog">
-                <h2>Changelog</h2>
-                <div class="changelog-version">
-                    <h3>Version 0.1.1</h3>
-                    <ul>
-                        <li>Added stats tables in item tooltip for a few item types which were missing them</li>
-                        <li>Fixed an issue where stats tables in item tooltips could extend beyond the tooltip boundaries</li>
-                    </ul>
-                </div>
-                <div class="changelog-version">
-                    <h3>Version 0.1.0</h3>
-                    <ul>
-                        <li>XCOM Companion first released</li>
-                    </ul>
-                </div>
+            <div class="changelog-version">
+                <h3>Version 0.1.0</h3>
+                <ul>
+                    <li>XCOM Companion first released</li>
+                </ul>
             </div>
         </div>
-    </section>
+    </div>
 </template>

commit 08fe92ac35a9656c1f91221517e17f3cd050e7e1

Fix bug introduced in 7fc6a17 where DOM elements are recreated while inserting pages into DOM

assets/js/page-manager.js
@@ -203,7 +203,8 @@ class PageManager {
         this._pageTitleHolder.innerHTML += pageDocument.title.text;
 
         // Clear out the contents of the hosting element and replace them with this page
-        this._pageContentHolder.innerHTML = documentBody.outerHTML;
+        this._pageContentHolder.innerHTML = "";
+        this._pageContentHolder.appendChild(documentBody);
     }
 
     _pushToHistory(historyState) {