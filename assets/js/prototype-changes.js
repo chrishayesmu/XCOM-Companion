2c78e201695f293163f05bc9abd5567730437c54
Chris Hayes <chrishayesmu@gmail.com>
2021-06-10 07:30:15 -0700

    Add a couple more utility methods to Math, and fix a potential representation issue in Math.roundTo

--- assets/js/prototype-changes.js
+++ assets/js/prototype-changes.js
@@ -9,9 +9,31 @@ Object.defineProperty(Array.prototype, "last", {
     }
 });
 
+Object.defineProperty(Array.prototype, "remove", {
+    value: function(item) {
+        const index = this.indexOf(item);
+
+        if (index >= 0) {
+            this.splice(index, 1);
+        }
+
+        return index >= 0;
+    }
+});
+
+Object.defineProperty(Math, "clamp", {
+    value: function(val, min, max) {
+        return Math.max(min, Math.min(val, max));
+    }
+});
+
 Object.defineProperty(Math, "roundTo", {
     value: function(val, numPlaces) {
         const powerOfTen = Math.pow(10, numPlaces);
-        return (Math.ceil(val * powerOfTen) / powerOfTen).toFixed(numPlaces);
+
+        // toFixed converts to a string; we turn it back into a float, both so it can have math done on it,
+        // and to drop any trailing zeroes from the string representation
+        const asString = (Math.ceil(val * powerOfTen) / powerOfTen).toFixed(numPlaces);
+        return parseFloat(asString);
     }
 });
