ade71e1814bd1cb57827648f0942fd1f31d20abf
Chris Hayes <chrishayesmu@gmail.com>
2021-09-15 11:16:52 -0700

    Fix getter in xcom-campaign returning the wrong field

--- assets/js/xcom-campaign.js
+++ assets/js/xcom-campaign.js
@@ -1326,7 +1326,7 @@ class XComCampaign {
     }
 
     get startingCountryBonusIndex() {
-        return this.#startingCountry;
+        return this.#startingCountryBonusIndex;
     }
 
     // #endregion
